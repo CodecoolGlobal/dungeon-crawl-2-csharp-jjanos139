62e16c013adc70458370be379cb903feb4922d9e
MagaiM <magai.marton@gmail.com>
2022-03-29 16:34:08 +0200

    Add movement random directions every sec.

--- Assets/Source/Actors/Characters/Skeleton.cs
+++ Assets/Source/Actors/Characters/Skeleton.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using static DungeonCrawl.Utilities;
 
 namespace DungeonCrawl.Actors.Characters
 {
@@ -14,6 +15,19 @@ namespace DungeonCrawl.Actors.Characters
             Debug.Log("Well, I was already dead anyway...");
         }
 
+        protected override void OnUpdate(float deltaTime)
+        {
+            _turnCounter += deltaTime;
+            if (_turnCounter >= 1)
+            {
+                _turnCounter = 0;
+                Direction direction = GetRandomDirection();
+                TryMove(direction);
+            }
+        }
+
+
+        private float _turnCounter;
         public override int DefaultSpriteId => 316;
         public override string DefaultName => "Skeleton";
     }
