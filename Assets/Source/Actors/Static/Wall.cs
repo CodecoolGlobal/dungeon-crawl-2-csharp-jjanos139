e7cc57bf52ece6377009fdb866448d676dfab65f
Phyxick <89603303+Phyxick@users.noreply.github.com>
2022-03-29 11:19:37 +0200

    player can't pass the wall

--- Assets/Source/Actors/Static/Wall.cs
+++ Assets/Source/Actors/Static/Wall.cs
@@ -1,8 +1,16 @@
-﻿namespace DungeonCrawl.Actors.Static
+﻿using DungeonCrawl.Actors.Characters;
+
+namespace DungeonCrawl.Actors.Static
 {
     public class Wall : Actor
     {
         public override int DefaultSpriteId => 825;
         public override string DefaultName => "Wall";
+        public override bool OnCollision(Actor anotherActor)
+        {
+            if (anotherActor.DefaultName == "Player")
+                return false;
+            return true;
+        }
     }
 }
