c14fbd006c3da67180cbf82ba254e02211480570
jjanos139 <jdale139@gmail.com>
2022-03-30 12:53:22 +0100

    Add DeathSounds to Player

--- Assets/Source/Actors/Characters/Player.cs
+++ Assets/Source/Actors/Characters/Player.cs
@@ -1,9 +1,43 @@
 ﻿using UnityEngine;
+using System;
 
 namespace DungeonCrawl.Actors.Characters
 {
     public class Player : Character
     {
+        public AudioSource _deathSound1;
+        public AudioSource _deathSound2;
+        public AudioSource _deathSound3;
+
+        System.Random randomSound = new System.Random();
+        private void Awake()
+        {
+            base.Awake();
+            InstantiateAudio();
+        }
+        void InstantiateAudio()
+        {
+            _deathSound1 = Instantiate(Resources.Load<AudioSource>("DeathSound1"));
+            _deathSound2 = Instantiate(Resources.Load<AudioSource>("DeathSound2"));
+            _deathSound3 = Instantiate(Resources.Load<AudioSource>("DeathSound3"));
+            _deathSound1.transform.parent = transform;
+            _deathSound2.transform.parent = transform;
+            _deathSound3.transform.parent = transform;
+        }
+
+        void PlayAudio()
+        {
+            int soundCase = randomSound.Next(1, 4);
+
+            switch (soundCase)
+            {
+                case 1: _deathSound1.Play(); break;
+                case 2: _deathSound2.Play(); break;
+                case 3: _deathSound3.Play(); break;
+            }
+        }
+
+            
         protected override void OnUpdate(float deltaTime)
         {
             if (Input.GetKeyDown(KeyCode.W))
@@ -38,10 +72,13 @@ namespace DungeonCrawl.Actors.Characters
 
         protected override void OnDeath()
         {
+            PlayAudio();
             Debug.Log("Oh no, I'm dead!");
         }
 
         public override int DefaultSpriteId => 24;
         public override string DefaultName => "Player";
+
+        
     }
 }
