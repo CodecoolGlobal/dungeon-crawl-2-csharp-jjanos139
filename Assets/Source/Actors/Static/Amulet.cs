ca435fb15fe22e8a2963ab15306908a74328a385
bosiTomi <bosanszkitamas@gmail.com>
2022-03-31 19:33:11 +0200

    fixed some more pickable stuff, new color for sprites

--- Assets/Source/Actors/Static/Amulet.cs
+++ Assets/Source/Actors/Static/Amulet.cs
@@ -1,4 +1,5 @@
-﻿using DungeonCrawl.Actors.Characters;
+﻿using Assets.Source.Core;
+using DungeonCrawl.Actors.Characters;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -21,7 +22,12 @@ namespace DungeonCrawl.Actors.Static
         }
         public override bool OnCollision(Actor anotherActor)
         {
-            if (anotherActor is Player)
+            if (anotherActor.DefaultName == "Player")
+            {
+                UserInterface.Singleton.SetText("Press E to pick up", UserInterface.TextPosition.BottomRight);
+                anotherActor.ItemUnder = this;
+            }
+            else if (anotherActor is Player)
                 _itemPickUp.Play();
             return true;
         }
