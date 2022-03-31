df54bb81d52d19723e64121e58c31a8c5ef42222
jjanos139 <jdale139@gmail.com>
2022-03-31 10:31:22 +0100

    Add Bear and Bat sounds

--- Assets/Source/Actors/Static/Amulet.cs
+++ Assets/Source/Actors/Static/Amulet.cs
@@ -29,7 +29,6 @@ namespace DungeonCrawl.Actors.Static
         public override string DefaultName => "Amulet";
         public override int Z => -1;
 
-
         public override bool Detectable => true;
     }
 }
