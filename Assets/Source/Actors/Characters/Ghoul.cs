4fae48c903e214c8b59e2027f66ffd962d6b5cd6
bosiTomi <bosanszkitamas@gmail.com>
2022-03-31 15:28:42 +0200

    added more moving mobs, little fix in map1

--- Assets/Source/Actors/Characters/Ghoul.cs
+++ Assets/Source/Actors/Characters/Ghoul.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using DungeonCrawl.Core;
+using UnityEngine;
+using static DungeonCrawl.Utilities;
 
 namespace DungeonCrawl.Actors.Characters
 {
@@ -8,6 +10,28 @@ namespace DungeonCrawl.Actors.Characters
         {
             return true;
         }
+        protected override void OnUpdate(float deltaTime)
+        {
+            _turnCounter += deltaTime;
+            if (_turnCounter >= 1)
+            {
+                _turnCounter = 0;
+                (int x, int y) playerCoords = ActorManager.Singleton.GetPlayer().Position;
+                CheckIfAggro(playerCoords, 2, 5);
+
+                if (_isAggro)
+                {
+                    TryMove(GetMoveDirection(playerCoords));
+                }
+                else if (!_isAggro)
+                {
+                    Direction direction = GetRandomDirection();
+                    TryMove(direction);
+                }
+            }
+        }
+
+        private float _turnCounter;
 
         protected override void OnDeath()
         {
