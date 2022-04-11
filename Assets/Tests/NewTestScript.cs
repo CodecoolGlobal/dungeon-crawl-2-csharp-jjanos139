de37baadfcec75459382c13cacd346cf72941c62
jjanos139 <jdale139@gmail.com>
2022-04-11 17:58:54 +0200

    Testing unity tests

---
+++ Assets/Tests/NewTestScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using DungeonCrawl.Actors.Characters;
+using DungeonCrawl.Core;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class NewTestScript
+{
+    [SetUp]
+    public void Setup()
+    {
+        MapLoader.LoadMap(4);
+    }
+
+    // A Test behaves as an ordinary method
+    [Test]
+    public void NewTestScriptSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator NewTestScriptWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+
+    [Test]
+    public void GetActorAtTest()
+    {
+        var result = ActorManager.Singleton.GetActorAt((1, -1));
+        Assert.AreEqual("Player", result.DefaultName);
+    }
+}
